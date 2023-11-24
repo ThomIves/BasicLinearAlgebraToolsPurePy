c82370afcc5fa397d0b062edbcc1ff27e12e4372
RandomGamingDev <83996185+RandomGamingDev@users.noreply.github.com>
2023-11-24 00:38:01 -0500

    Fixed `determinant_fast()`

--- LinearAlgebraPurePython.py
+++ LinearAlgebraPurePython.py
@@ -327,8 +327,14 @@ def determinant_fast(A):
 
     # Section 2: Row manipulate A into an upper triangle matrix
     for fd in range(n):  # fd stands for focus diagonal
-        if AM[fd][fd] == 0:
-            AM[fd][fd] = 1.0e-18  # Cheating by adding zero + ~zero
+        if AM[fd][fd] == 0: # Do a row swap with a row that won't result in a pivot of 0
+            for i in range(fd+1,n):
+                if AM[i][fd] != 0:
+                    temp = AM[fd]
+                    AM[fd] = AM[i]
+                    AM[i] = temp
+                    break
+            return 0 # If there are no other rows that can do that then return the determinant of 0
         for i in range(fd+1, n):  # skip row with fd in it.
             crScaler = AM[i][fd] / AM[fd][fd]  # cr stands for "current row".
             for j in range(n):  # cr - crScaler * fdRow, one element at a time.
